c0c07bf0e96a88a73e5e66e5c5a79d7851361263
Bence Skroka <skorka.bence@gmail.com>
2016-12-08 10:27:33 +0100

    File check and default file

---
+++ megoldasom.ps1
@@ -0,0 +1,11 @@
+﻿if (!($args.Count -eq 1)) {
+    Write-Host "Egy parameter szukseges: File neve"
+    exit
+}
+
+$file = $args[0];
+
+if (!(Test-Path -PathType Leaf $file)) {
+    Write-Host "File nem letezik: $file"
+    exit
+}
